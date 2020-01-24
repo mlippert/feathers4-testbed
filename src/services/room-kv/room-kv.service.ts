e77b38372e2ed3b134bad1d0b2136adc31d8e33f
Mike Lippert <mlippert255@gmail.com>
2020-01-24 17:50:49 -0500

    generate service roomKV
    ```
    $ npx feathers generate service
    ? What kind of service is it? Mongoose
    ? What is the name of the service? roomKV
    ? Which path should the service be registered on? /room-kv
    ? Does the service require authentication? Yes
    ```

---
+++ src/services/room-kv/room-kv.service.ts
@@ -0,0 +1,28 @@
+// Initializes the `roomKV` service on path `/room-kv`
+import { ServiceAddons } from '@feathersjs/feathers';
+import { Application } from '../../declarations';
+import { RoomKv } from './room-kv.class';
+import createModel from '../../models/room-kv.model';
+import hooks from './room-kv.hooks';
+
+// Add this service to the service type index
+declare module '../../declarations' {
+  interface ServiceTypes {
+    'room-kv': RoomKv & ServiceAddons<any>;
+  }
+}
+
+export default function (app: Application) {
+  const options = {
+    Model: createModel(app),
+    paginate: app.get('paginate')
+  };
+
+  // Initialize our service with any options it requires
+  app.use('/room-kv', new RoomKv(options, app));
+
+  // Get our initialized service so that we can register hooks
+  const service = app.service('room-kv');
+
+  service.hooks(hooks);
+}
